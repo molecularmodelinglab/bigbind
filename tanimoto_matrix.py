610088e4c6d81106dbcbe0511cfc5a80997072de
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-06-13 18:03:52 -0400

    running tanimoto matrix calc on longleaf

--- tanimoto_matrix.py
+++ tanimoto_matrix.py
@@ -1,9 +1,11 @@
-from multiprocessing import Pool
+from functools import partial
+from multiprocessing import Pool, shared_memory
 import random
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import numpy as np
 import pandas as pd
+from scipy import sparse
 from tqdm import tqdm
 import yaml
 from cache import cache
@@ -32,11 +34,58 @@ def get_morgan_fps_parallel(cfg, df):
         fps = list(tqdm(p.imap(get_fp, df.lig_smiles), total=len(df)))
     fps = np.asarray(fps)
     return fps
-    
+
+def batch_tanimoto(fp, fps):
+    inter = np.logical_and(fp, fps)
+    union = np.logical_or(fp, fps)
+    sims = inter.sum(-1)/union.sum(-1)
+    return sims
+
+def batch_tanimoto_faster(fp_shape, fp_shm_name, fp_sum_shape, fp_sum_shm_name, idx):
+    fp_shm = shared_memory.SharedMemory(name=fp_shm_name)
+    fps = np.ndarray(fp_shape, dtype=bool, buffer=fp_shm.buf)
+
+    fp_sum_shm = shared_memory.SharedMemory(name=fp_sum_shm_name)
+    fp_sum = np.ndarray(fp_sum_shape, dtype=bool, buffer=fp_sum_shm.buf)
+
+    fp = fps[idx]
+    inter = np.logical_and(fp, fps)
+    inter_sum = inter.sum(-1)
+    sims = inter_sum/(fp_sum + fp.sum() - inter_sum)
+
+    sims[sims < 0.2] = 0.0
+    ssim = sparse.csc_matrix(sims)
+
+    return ssim
+
+@cache
+def get_tanimoto_matrix(cfg, fps):
+    try:
+        fp_sum = fps.sum(-1)
+
+        fp_shm = shared_memory.SharedMemory(create=True, size=fps.nbytes)
+        fps_shared = np.ndarray(fps.shape, dtype=bool, buffer=fp_shm.buf)
+        fps_shared[:] = fps[:]
+        
+        fp_sum_shm = shared_memory.SharedMemory(create=True, size=fp_sum.nbytes)
+        fp_sum_shared = np.ndarray(fp_sum.shape, dtype=bool, buffer=fp_sum_shm.buf)
+        fp_sum_shared[:] = fp_sum[:]
+
+        sim_func = partial(batch_tanimoto_faster, fps.shape, fp_shm.name, fp_sum.shape, fp_sum_shm.name)
+        with Pool(cfg['num_cpus']) as p:
+            cols = list(tqdm(p.imap(sim_func, range(len(fps))), total=len(fps)))
+        return sparse.vstack(cols)
+    finally:
+        fp_shm.close()
+        fp_shm.unlink()
+        fp_sum_shm.close()
+        fp_sum_shm.unlink()
+
 def main(cfg):
     df = pd.read_csv(f"{cfg['bigbind_folder']}/activities_all.csv")
     fps = get_morgan_fps_parallel(cfg, df)
-    print(len(fps))
+    tm = get_tanimoto_matrix(cfg, fps)
+    print(tm.shape)
 
 SEED = 49
 random.seed(SEED)
---
+++ tanimoto_matrix.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -J tanimoto_matrix
+#SBATCH -t 2-00:00:00
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16G
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/work/users/m/i/mixarcid/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/work/users/m/i/mixarcid/mixarcid/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/work/users/m/i/mixarcid/mixarcid/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/work/users/m/i/mixarcid/mixarcid/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+module load gcc/11.2.0
+module load cuda/11.8
+conda activate plantain-nightly
+
+cd /nas/longleaf/home/mixarcid/bigbind
+
+python tanimoto_matrix.py
