e134d96476a1d7ba8231d98eda79d414ab0b3dbe
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-08-30 13:08:50 -0400

    submitting to slurm

--- slurm.py
+++ slurm.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
-from tempfile import NamedTemporaryFile
 import subprocess
+import os
 
 def submit_slurm_task(cfg, workflow, node):
     index = workflow.nodes.index(node)
@@ -9,14 +9,18 @@ def submit_slurm_task(cfg, workflow, node):
     hours, remainder = divmod(runtime.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
+    log_dir = os.path.join(cfg.host.work_dir, "logs")
+    out_file = os.path.join(log_dir, task_name + ".out")
+    err_file = os.path.join(log_dir, task_name + ".err")
+
     sbatch_args = []
     sbatch_args.append(f"-J {node.task.name}")
     sbatch_args.append(f"-t {runtime.days:02d}-{int(hours):02}:{int(minutes):02}:{int(seconds):02}")
     sbatch_args.append(f"--cpus-per-task={node.task.n_cpu}")
     sbatch_args.append(f"--mem={node.task.mem}G")
+    sbatch_args.append(f"--output={out_file}")
+    sbatch_args.append(f"--error={err_file}")
     sbatch_args = " ".join(sbatch_args)
-
-    # with NamedTemporaryFile("") as 
 
     run_cmds = []
     if "pre_command" in cfg.host:
