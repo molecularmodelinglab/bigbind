31d54acf7db3c1206e9c59704103ce3dde0231f6
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-11-08 07:53:46 -0500

    removed old stuff

--- baselines/eef.py
+++ baselines/eef.py
@@ -41,10 +41,15 @@ class CutoffExpDist:
 
 A_dist = CutoffExpDist(None, 3.5, 0.3, 0.05)
 
-def calc_eef(act_preds, rand_preds, activities, act_cutoff, select_num=None, select_frac=None):
-    """" Computes expected enrichment factor (EEF)
-    at a particular percentage. TODO: scale the probabilities
-    by the activities. """
+def calc_eef(act_preds, rand_preds, activities, act_cutoff, select_frac):
+    """" Computes expected enrichment factor (EEF) at a particular percentage.
+    :param act_preds: predictions for the "active" compounds
+        (though we reduce this number according to act_cutoff)
+    :param rand_preds: predictions for the random compounds
+    :param activities: the actual activities of the active compounds (pchembl_value)
+    :param act_cutoff: the cutoff for what is considered an active compound (5 in the paper)
+    :param select_frac: top fraction of compounds selected. E.g. 0.01 for EEF_1%
+    :returns: Tuple of (EEF, lower bound, upper bound, p-value that EEF > 1)"""
 
     # assert select_num is None
     # select_num = int((1 - select_frac) * len(rand_preds)) + 1
@@ -57,16 +62,17 @@ def calc_eef(act_preds, rand_preds, activities, act_cutoff, select_num=None, sel
     select = sorted(rand_preds)[select_num-1]
     is_active = activities >= act_cutoff
 
-    # weigh each active according to the probability of its activity
+    # We don't weigh each active according to the probability
+    # of its activity (though this may be a good idea in the future )
     # alphas = A_dist.pdf(activities[is_active])
     # alphas = alphas/alphas.sum()
+    # P_act = (alphas * (act_preds >= select)[is_active]).sum()
 
     P_rand = (rand_preds >= select).sum()/len(rand_preds)
     
     K = ((act_preds >= select) & is_active).sum()
     N = is_active.sum()
     P_act = K/N
-    # P_act = (alphas * (act_preds >= select)[is_active]).sum()
 
     ef_hat = P_act/P_rand
 
@@ -96,58 +102,3 @@ def calc_best_eef(preds, true_act, act_cutoff):
         if cur_best is None or eef > cur_best[0]:
             cur_best = (eef, high, low, pval, cur_frac, cur_N)
     return cur_best
-
-
-ignore_pockets = { "AL1A1_HUMAN_4_501_0", "ESR1_HUMAN_300_553_0", "CBP_HUMAN_1079_1197_0" }
-_valid_indexes = None
-def get_all_valid_indexes(cfg):
-    """ Alas, we created the bayesbind set (and docked everything) before realizing
-    that we need to remove all potency (HTS) values from the activities. So now
-    we postprocess everything smh"""
-    global _valid_indexes
-    if _valid_indexes is not None:
-        return _valid_indexes
-    
-    _valid_indexes = {}
-    for split, pocket in get_all_bayesbind_splits_and_pockets(cfg):
-        if pocket in ignore_pockets:
-            continue
-        df = pd.read_csv(get_bayesbind_dir(cfg) + f"/{split}/{pocket}/actives.csv")
-        if len(df.query("standard_type != 'Potency' and pchembl_value > 5")) > 30:
-            _valid_indexes[pocket] = df.query("standard_type != 'Potency'").index
-    return _valid_indexes
-
-def postprocess_preds(cfg, preds):
-    """ Postprocess predictions on old (including potency) bayesbind set so that
-    they now only predict non-potency values """
-    valid_indexes = get_all_valid_indexes(cfg)
-    ret = {}
-    for pocket in preds:
-        if pocket in valid_indexes:
-            ret[pocket] = {}
-            ret[pocket]["random"] = preds[pocket]["random"]
-            ret[pocket]["actives"] = preds[pocket]["actives"][valid_indexes[pocket]]
-    return ret
-
-def get_all_bayesbind_activities(cfg):
-    valid_indexes = get_all_valid_indexes(cfg)
-    poc2activities = {}
-    for folder in glob(get_bayesbind_dir(cfg) + "/*/*"):
-        pocket = folder.split("/")[-1]
-        if pocket in valid_indexes:
-            poc2activities[pocket] = pd.read_csv(folder + "/actives.csv").pchembl_value[valid_indexes[pocket]]
-    return poc2activities
-
-def get_all_metrics(cfg, predictions, act_cutoff=5, N=None, frac=None):
-    poc2activities = get_all_bayesbind_activities(cfg)
-    ret = {}
-    all_eefs = []
-    for pocket, preds in predictions.items():
-        eef, low, high = calc_eef(preds["actives"], preds["random"], poc2activities[pocket], act_cutoff, N, frac)
-        all_eefs.append(eef)
-        ret[f"{pocket}_EEF_{N}"] = eef
-        ret[f"{pocket}_EEF_{N}_high"] = high
-        ret[f"{pocket}_EEF_{N}_low"] = low
-    ret[f"mean_EEF_{N}"] = np.mean(all_eefs)
-    ret[f"median_EEF_{N}"] = np.median(all_eefs)
-    return ret
--- baselines/evaluate.py
+++ baselines/evaluate.py
@@ -9,7 +9,7 @@ import pandas as pd
 import scipy
 from tqdm import tqdm, trange
 from rdkit.Chem import PandasTools
-from baselines.eef import calc_best_eef, calc_eef, get_all_bayesbind_activities, postprocess_preds
+from baselines.eef import calc_best_eef, calc_eef
 from baselines.knn import get_all_bayesbind_knn_preds
 from baselines.vina_gnina import get_all_bayesbind_splits_and_pockets
 from baselines.banana import run_all_banana
@@ -18,6 +18,59 @@ from utils.cfg_utils import get_baseline_dir, get_bayesbind_dir, get_config
 from utils.task import task
 from utils.workflow import Workflow
 
+ignore_pockets = { "AL1A1_HUMAN_4_501_0", "ESR1_HUMAN_300_553_0", "CBP_HUMAN_1079_1197_0" }
+_valid_indexes = None
+def get_all_valid_indexes(cfg):
+    """ Alas, we created the bayesbind set (and docked everything) before realizing
+    that we need to remove all potency (HTS) values from the activities. So now
+    we postprocess everything smh"""
+    global _valid_indexes
+    if _valid_indexes is not None:
+        return _valid_indexes
+    
+    _valid_indexes = {}
+    for split, pocket in get_all_bayesbind_splits_and_pockets(cfg):
+        if pocket in ignore_pockets:
+            continue
+        df = pd.read_csv(get_bayesbind_dir(cfg) + f"/{split}/{pocket}/actives.csv")
+        if len(df.query("standard_type != 'Potency' and pchembl_value > 5")) > 30:
+            _valid_indexes[pocket] = df.query("standard_type != 'Potency'").index
+    return _valid_indexes
+
+def postprocess_preds(cfg, preds):
+    """ Postprocess predictions on old (including potency) bayesbind set so that
+    they now only predict non-potency values """
+    valid_indexes = get_all_valid_indexes(cfg)
+    ret = {}
+    for pocket in preds:
+        if pocket in valid_indexes:
+            ret[pocket] = {}
+            ret[pocket]["random"] = preds[pocket]["random"]
+            ret[pocket]["actives"] = preds[pocket]["actives"][valid_indexes[pocket]]
+    return ret
+
+def get_all_bayesbind_activities(cfg):
+    valid_indexes = get_all_valid_indexes(cfg)
+    poc2activities = {}
+    for folder in glob(get_bayesbind_dir(cfg) + "/*/*"):
+        pocket = folder.split("/")[-1]
+        if pocket in valid_indexes:
+            poc2activities[pocket] = pd.read_csv(folder + "/actives.csv").pchembl_value[valid_indexes[pocket]]
+    return poc2activities
+
+def get_all_metrics(cfg, predictions, act_cutoff=5, N=None, frac=None):
+    poc2activities = get_all_bayesbind_activities(cfg)
+    ret = {}
+    all_eefs = []
+    for pocket, preds in predictions.items():
+        eef, low, high = calc_eef(preds["actives"], preds["random"], poc2activities[pocket], act_cutoff, N, frac)
+        all_eefs.append(eef)
+        ret[f"{pocket}_EEF_{N}"] = eef
+        ret[f"{pocket}_EEF_{N}_high"] = high
+        ret[f"{pocket}_EEF_{N}_low"] = low
+    ret[f"mean_EEF_{N}"] = np.mean(all_eefs)
+    ret[f"median_EEF_{N}"] = np.median(all_eefs)
+    return ret
 
 def get_docked_scores_from_pdbqt(fname):
     return PDBQTMolecule.from_file(fname)._pose_data["free_energies"]
