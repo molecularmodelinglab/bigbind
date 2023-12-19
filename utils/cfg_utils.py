aa92eb19f38c30ed29676c22bd5e69e47944c148
Michael Arcidiacono <mixarcidiacono@gmail.com>
2023-12-19 14:19:46 -0500

    prepping ligs for struct

--- utils/cfg_utils.py
+++ utils/cfg_utils.py
@@ -30,6 +30,10 @@ def get_figure_dir(cfg):
 def get_parent_baseline_dir(cfg):
     return os.path.join(cfg.host.work_dir, cfg.run_name, "global", "baselines")
 
+def get_parent_baseline_struct_dir(cfg):
+    return os.path.join(cfg.host.work_dir, cfg.run_name, "global", "baselines_struct")
+
+
 def get_baseline_dir(cfg, program_name, split, pocket):
     ret = os.path.join(cfg.host.work_dir, cfg.run_name, "global", "baselines", program_name, split, pocket)
     os.makedirs(ret, exist_ok=True)
